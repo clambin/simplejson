fa2783754d12ad13ea2688b1313ee0cf657ba0c9
clambin <christophe.lambin@gmail.com>
2021-10-22 18:45:09 +0200

    feat: TargetTable convenience function to run queries

--- targettable.go
+++ targettable.go
@@ -1,6 +1,10 @@
 package grafana_json
 
-import "sort"
+import (
+	"context"
+	"fmt"
+	"sort"
+)
 
 // TargetTable is a convenience struct for handlers that support multiple targets
 type TargetTable map[string]struct {
@@ -16,4 +20,24 @@ func (tt TargetTable) Targets() (targets []string) {
 	}
 	sort.Strings(targets)
 	return
+}
+
+func (tt TargetTable) RunQuery(ctx context.Context, target string, args *TimeSeriesQueryArgs) (response *QueryResponse, err error) {
+	builder, ok := tt[target]
+
+	if ok == false || builder.QueryFunc == nil {
+		return nil, fmt.Errorf("unknown target '%s' for TimeSeries Query", target)
+	}
+
+	return builder.QueryFunc(ctx, target, args)
+}
+
+func (tt TargetTable) RunTableQuery(ctx context.Context, target string, args *TableQueryArgs) (response *TableQueryResponse, err error) {
+	builder, ok := tt[target]
+
+	if ok == false || builder.TableQueryFunc == nil {
+		return nil, fmt.Errorf("unknown target '%s' for Table Query", target)
+	}
+
+	return builder.TableQueryFunc(ctx, target, args)
 }
--- targettable_test.go
+++ targettable_test.go
@@ -2,13 +2,14 @@ package grafana_json_test
 
 import (
 	"context"
+	"fmt"
 	grafanajson "github.com/clambin/grafana-json"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestTargetTable_Targets(t *testing.T) {
-	tt := grafanajson.TargetTable{
+var (
+	tt = grafanajson.TargetTable{
 		"empty": {},
 		"query": {
 			QueryFunc: queryFunc,
@@ -21,14 +22,40 @@ func TestTargetTable_Targets(t *testing.T) {
 			TableQueryFunc: tableQueryFunc,
 		},
 	}
+)
 
+func TestTargetTable_Targets(t *testing.T) {
 	assert.Equal(t, []string{"both", "query", "tablequery"}, tt.Targets())
 }
 
+func TestTargetTable_RunQuery(t *testing.T) {
+	_, err := tt.RunQuery(context.Background(), "query", &grafanajson.TimeSeriesQueryArgs{})
+	assert.Equal(t, "not implemented", err.Error())
+
+	_, err = tt.RunQuery(context.Background(), "tablequery", &grafanajson.TimeSeriesQueryArgs{})
+	assert.Equal(t, "unknown target 'tablequery' for TimeSeries Query", err.Error())
+
+	_, err = tt.RunQuery(context.Background(), "invalid", &grafanajson.TimeSeriesQueryArgs{})
+	assert.Equal(t, "unknown target 'invalid' for TimeSeries Query", err.Error())
+}
+
+func TestTargetTable_RunTableQuery(t *testing.T) {
+	_, err := tt.RunTableQuery(context.Background(), "tablequery", &grafanajson.TableQueryArgs{})
+	assert.Equal(t, "not implemented", err.Error())
+
+	_, err = tt.RunTableQuery(context.Background(), "query", &grafanajson.TableQueryArgs{})
+	assert.Equal(t, "unknown target 'query' for Table Query", err.Error())
+
+	_, err = tt.RunTableQuery(context.Background(), "invalid", &grafanajson.TableQueryArgs{})
+	assert.Equal(t, "unknown target 'invalid' for Table Query", err.Error())
+}
+
 func queryFunc(_ context.Context, _ string, _ *grafanajson.TimeSeriesQueryArgs) (response *grafanajson.QueryResponse, err error) {
+	err = fmt.Errorf("not implemented")
 	return
 }
 
 func tableQueryFunc(_ context.Context, _ string, _ *grafanajson.TableQueryArgs) (response *grafanajson.TableQueryResponse, err error) {
+	err = fmt.Errorf("not implemented")
 	return
 }
