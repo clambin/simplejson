57e7e793f17257203355f6b09117efeb90f2e8d8
clambin <christophe.lambin@gmail.com>
2022-05-28 13:15:51 +0200

    fix: address security recommendation

--- endpoint.go
+++ endpoint.go
@@ -2,44 +2,32 @@ package simplejson
 
 import (
 	"encoding/json"
-	log "github.com/sirupsen/logrus"
-	"io"
 	"net/http"
 )
 
 // handleEndpoint is a wrapper for simplejson endpoint handlers. It parses the incoming http.Request, calls the processor
-// with that request and writes the response to the http.ResponseWriter.
+// and writes the response to the http.ResponseWriter.
 func handleEndpoint(w http.ResponseWriter, req *http.Request, request json.Unmarshaler, processor func() ([]json.Marshaler, error)) {
-	body, err := io.ReadAll(req.Body)
-
-	if err == nil && len(body) > 0 {
-		log.Debugf("request: %s", string(body))
-		err = json.Unmarshal(body, request)
-	}
-
-	if err != nil {
-		http.Error(w, "failed to parse request: "+err.Error(), http.StatusBadRequest)
-		return
+	var err error
+	if req.ContentLength > 0 {
+		if err = json.NewDecoder(req.Body).Decode(&request); err != nil {
+			http.Error(w, "failed to parse request: "+err.Error(), http.StatusBadRequest)
+			return
+		}
 	}
 
 	var response interface{}
-	response, err = processor()
-
-	if err != nil {
+	if response, err = processor(); err != nil {
 		http.Error(w, "failed to process request: "+err.Error(), http.StatusInternalServerError)
 		return
 
 	}
 
 	var output []byte
-	output, err = json.Marshal(response)
-
-	if err != nil {
+	if output, err = json.Marshal(response); err != nil {
 		http.Error(w, "failed to create response: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	log.Debugf("response: %s", string(output))
 
 	_, _ = w.Write(output)
 }
